c4a144becfef26ebbc69d6e5c2f3352988e34d4c
shaoyuWang <46708683+shaoyuWang@users.noreply.github.com>
2019-05-18 13:12:11 +0800

    update .gitignore

---
+++ server/server-src/modules/main/services/goumet.master.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from "@nestjs/common";
+import { InjectRepository } from "@nestjs/typeorm";
+import { Repository } from "typeorm";
+import _ from "lodash";
+import { RESPONSE_CODE } from "../../../framework/enums";
+import { UserEntity } from "../../admin/entities";
+
+@Injectable()
+export class GoumetMasterService {
+  constructor(
+    @InjectRepository(UserEntity)
+    private readonly userRepository: Repository<UserEntity>,
+  ) {}
+
+  //   获取列表
+  public async getList() {
+    const users = await this.userRepository.find();
+    if (!_.isEmpty(users)) {
+      return { data: users, code: RESPONSE_CODE.SUCCESS };
+    } else {
+      return { code: RESPONSE_CODE.NOTVALUE };
+    }
+  }
+}
